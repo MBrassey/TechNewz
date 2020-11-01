af80762bed49f021afb9bd2749847aef92edc359
luc1dLife <luc1d_life@protonmail.com>
2020-11-01 01:11:27 -0600

    + Login Functionality

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Initialize Application
 init = () => {
