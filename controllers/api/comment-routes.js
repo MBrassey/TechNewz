2e1342194aa1ff8cecb6bdb343b20b21228fd6ee
luc1dLife <luc1d_life@protonmail.com>
2020-11-02 02:32:52 -0700

    Conditionally rendered elements

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,16 +13,20 @@ router.get("/", (req, res) => {
 
 // Post New Comment
 router.post("/", (req, res) => {
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.body.user_id,
-    post_id: req.body.post_id,
-  })
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // check the session
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
 // Delete Comment by Id
